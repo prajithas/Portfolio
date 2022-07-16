2283a809758f0b8e61dc69618e0fffcb7c7a34d9
Prajitha <98685133+prajithas@users.noreply.github.com>
2022-07-16 16:53:53 +0530

    Update testimonial.service.ts

--- Frontend/src/app/testimonial.service.ts
+++ Frontend/src/app/testimonial.service.ts
@@ -5,9 +5,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TestimonialService {
-
+  server_address:string='api';
   constructor(private http:HttpClient) { }
   getTestimonials(){
-    return this.http.get("http://localhost:3000/testimonials");
+    return this.http.get(`${this.server_address}/testimonials`);
   }
 }
